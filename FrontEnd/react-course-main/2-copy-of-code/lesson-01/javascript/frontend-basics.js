c2ab97040a7e10b46404b97293597c6fe3991682
bharathb007 <bharath.b@clario.com>
2026-03-04 19:36:38 +0530

    done

--- FrontEnd/react-course-main/2-copy-of-code/lesson-01/javascript/frontend-basics.js
+++ FrontEnd/react-course-main/2-copy-of-code/lesson-01/javascript/frontend-basics.js
@@ -1,31 +1,28 @@
 
 // Calculate and display the result (2 + 2)
-// document.getElementById('calculation').textContent = 2 + 2;
-// document.querySelector('.myclass').textContent = 2 * 2;
+document.getElementById('calculation').textContent = 2 + 2;
+document.querySelector('.myclass').textContent = 2 * 2;
 
-// function sumbitme() {
-//   alert('Button clicked!');
-// }
+function sumbitme() {
+  alert('Button clicked!');
+}
 
-// document.querySelector('button').addEventListener('click', sumbitme);
-
-
-
-//  const div = document.createElement('div');
-//  const img = document.createElement('img');
-//     img.src = 'robot.png';
-//     img.width = 50;
-//     div.appendChild(img);
-//     document.getElementById('imghere').appendChild(div);
+document.querySelector('button').addEventListener('click', sumbitme);
 
 
 
-  
-  document.getElementById('myInput').oninput = function() {
-    let display = this.value;
-    console.log(display);
-    document.getElementById('inputDisplay').textContent = display;
-  };
-  
+ const div = document.createElement('div');
+ const img = document.createElement('img');
+    img.src = 'robot.png';
+    img.width = 50;
+    div.appendChild(img);
+    document.getElementById('imghere').appendChild(div);
+
+
+
+
+  function updateColor(color) {
+    document.getElementById('colorDisplay').textContent = 'My favorite color is ' + color ;
+  }
 
 

1b5ba3d06469e4e4b8dd50e694e9ba321bce634b
bharathb007 <bharath.b@clario.com>
2026-03-10 22:01:53 +0530

    fh

---
+++ zztypescript/test.ts
@@ -0,0 +1,33 @@
+const message: string = "Hello from TypeScript!";
+console.log(message);
+
+
+//npx tsc test.ts && node test.js
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export {}; // makes this file a module — fixes "Cannot redeclare" error
